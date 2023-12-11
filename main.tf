4554d5c96c2af62a58b3300842ec76ccd9dfa313
rannaladasu <rannaladasu@paloaltonetworks.com>
2023-12-11 12:52:18 -0600

    test env

--- main.tf
+++ main.tf
@@ -29,30 +29,30 @@ provider "prismacloud" {
 #### Prisma Cloud Custom Policies ####################################
 
 # Configure a custom build policy from a local file
-# resource "prismacloud_policy" "bPolicy" {
-#   name        = "Ensure resources are only created in permitted locations"
-#   policy_type = "config"
-#   cloud_type  = "azure"
-#   severity    = "high"
-#   labels      = []
-#   description = ""
-#   recommendation = ""
-#   rule {
-#     name = "Ensure resources are only created in permitted locations"
-#     rule_type = "Config"
-#     parameters = {
-#       savedSearch = false
-#       withIac     = true
-#     }
-#     children {
-#       type           = "build"
-#       recommendation = "fix it"
-#       metadata = {
-#         "code" : file("policies/aks/aks001.yaml"),
-#       }
-#     }
-#   }
-# } 
+resource "prismacloud_policy" "bPolicy" {
+  name        = "Ensure resources are only created in permitted locations"
+  policy_type = "config"
+  cloud_type  = "azure"
+  severity    = "high"
+  labels      = []
+  description = ""
+  recommendation = ""
+  rule {
+    name = "Ensure resources are only created in permitted locations"
+    rule_type = "Config"
+    parameters = {
+      savedSearch = false
+      withIac     = true
+    }
+    children {
+      type           = "build"
+      recommendation = "fix it"
+      metadata = {
+        "code" : file("policies/aks/aks001.yaml"),
+      }
+    }
+  }
+} 
 
 # # Configure a custom run policy from a local file
 
@@ -86,35 +86,35 @@ provider "prismacloud" {
 
 
 # # Configure a custom run and build policy from a local file
-resource "prismacloud_policy" "brPolicy" {
-  name        = "custom - sample run & build policy with remediation created with terraform"
-  policy_type = "config"
-  cloud_type  = "aws"
-  severity    = "high"
-  labels      = []
-  description = ""
-  rule {
-    name = "custom - sample run & build policy with remediation created with terraform"
-    rule_type = "Config"
-    parameters = {
-      savedSearch = false
-      withIac     = true
-    }
-    criteria = file("policies/aws/run_policy.rql")
-    children {
-      type           = "build"
-      recommendation = "fix it"
-      metadata = {
-        "code" : file("policies/aks/aks002.yaml"),
-      }
-    }
-  }
+# resource "prismacloud_policy" "brPolicy" {
+#   name        = "custom - sample run & build policy with remediation created with terraform"
+#   policy_type = "config"
+#   cloud_type  = "aws"
+#   severity    = "high"
+#   labels      = []
+#   description = ""
+#   rule {
+#     name = "custom - sample run & build policy with remediation created with terraform"
+#     rule_type = "Config"
+#     parameters = {
+#       savedSearch = false
+#       withIac     = true
+#     }
+#     criteria = file("policies/aws/run_policy.rql")
+#     children {
+#       type           = "build"
+#       recommendation = "fix it"
+#       metadata = {
+#         "code" : file("policies/aks/aks002.yaml"),
+#       }
+#     }
+#   }
 
-   remediation {
-   cli_script_template = "aws iam update-account-password-policy --minimum-password-length 14 --require-uppercase-characters --require-lowercase-characters --require-numbers --require-symbols --allow-users-to-change-password --password-reuse-prevention 24 --max-password-age 90"
-   description = "This CLI command requires 'iam:UpdateAccountPasswordPolicy' permission. Successful execution will update the password policy to set the minimum password length to 14, require lowercase, uppercase, symbol, allow users to reset password, cannot reuse the last 24 passwords and password expiration to 90 days."
-  }
-} 
+#    remediation {
+#    cli_script_template = "aws iam update-account-password-policy --minimum-password-length 14 --require-uppercase-characters --require-lowercase-characters --require-numbers --require-symbols --allow-users-to-change-password --password-reuse-prevention 24 --max-password-age 90"
+#    description = "This CLI command requires 'iam:UpdateAccountPasswordPolicy' permission. Successful execution will update the password policy to set the minimum password length to 14, require lowercase, uppercase, symbol, allow users to reset password, cannot reuse the last 24 passwords and password expiration to 90 days."
+#   }
+# } 
 
 
 
